52589bdee973f80a73851e297ad114a8fde6bcd7
Jesus Villarreal <jesusvs1100@gmail.com>
2023-03-01 17:54:40 -0600

    Commit de las 6

--- components-1/src/App.vue
+++ components-1/src/App.vue
@@ -1,13 +1,29 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue'
 import BlogPost from './components/BlogPost.vue'
+import {ref} from 'vue'
+
+const posts = ref([
+  {title: "Post 1", id: 1,body:"descripcion 1", btntext:"Alex"},
+  {title: "Post: 2", id: 2,body:"descripcion: 2", btntext:"Yisus"},
+  {title: "Post 3", id: 3,body:"descripcion 3", btntext:"Maylon"},
+  {title: "Post 4", id: 4, btntext:"Puchis"},
+])
+
 </script>
 
 <template>
   <ButtonCounter/>
-  <BlogPost title="Yisus" btntext="Flow"/>
-  <BlogPost title="Maylon" btntext="Mas"/>
-  <BlogPost title="Alex" btntext="Cash"/>
+  <BlogPost
+  v-for="post in posts"
+  :key="post.id"
+   :title="post.title"
+    :btntext="post.btntext"
+      :body="post.body"
+        :id="post.id"
+    >
+    </BlogPost>
+
 
 
 </template>
--- components-1/src/components/BlogPost.vue
+++ components-1/src/components/BlogPost.vue
@@ -1,11 +1,21 @@
 <script setup>
-defineProps(['title','btntext'])
+import {ref} from 'vue'
+defineProps({
+title: String,
+body: {
+    type: String,
+    default: "sin descripcion",
+},
+id: Number,
+btntext: String
+ })
 </script>
 <template>
     <div class="card">
     <div class="card-body">
         <h1 class="card-title">{{ title }}</h1>
-            <h5 class="card-text">Lorem ipsum dolor sit amet.</h5>
+            <h5 class="card-text">{{ body }}</h5>
+                <p class="">{{ id }}</p>
                 <button class="btn btn-info">{{btntext}}</button>
     </div>
     </div>
